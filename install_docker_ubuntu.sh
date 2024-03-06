991518badc865415a3126f2466e32aaadd93d6b2
ikamii <>
2024-03-06 09:01:20 +0100

    Docker Ubuntu Installation

---
+++ install_docker_ubuntu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install Docker on Ubuntu
+
+sudo apt update
+sudo apt install ca-certificates curl gnupg lsb-release
+
+# Register gpg keyring
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add package
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
